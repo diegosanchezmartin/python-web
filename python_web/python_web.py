812569788b1add0cdae12e3c4d740d0179704992
Diego Sánchez Martín <diego.sanchez@wocu-monitoring.com>
2024-07-03 17:10:01 +0200

    Fix error with import header path

--- python_web/python_web.py
+++ python_web/python_web.py
@@ -1,6 +1,6 @@
 import reflex as rx
 from python_web.components.navbar import navbar
-from python_web.views.header import header
+from python_web.views.header.header import header
 class State(rx.State):
     pass
 
