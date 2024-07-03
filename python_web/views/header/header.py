edb8bcc8e606af4a0e5fba45f9cfb08df6281ad5
Diego Sánchez Martín <diego.sanchez@wocu-monitoring.com>
2024-07-03 17:29:41 +0200

    Change the header format and add more text components

--- python_web/views/header/header.py
+++ python_web/views/header/header.py
@@ -2,5 +2,11 @@ import reflex as rx
 
 def header() -> rx.Component:
     return rx.vstack(
-        rx.avatar(fallback="Diego Sánchez Martín", size="6")
+        rx.avatar(fallback="DSM", size="6"),
+        rx.text("@diegosanchezmartin"),
+        rx.text("Buenas! 👋🏽 Me llamo Diego Sánchez Martín"),
+        rx.text("""Soy ingeniero de software con una maestría en inteligencia artificial.
+                Actualmente trabajo como QA Engineer en multiples aplicaciones: 
+                Aplicaciones de escritorio, desarrollo web, firmware y plataformas móviles.
+                Aquí podrás encontrar todos mis enlaces de interés ¡Bienvenid@! """)
     )
