862984dbb90ae73475cc6ef6df7c4b3bc9066af7
Diego Sánchez Martín <diego.sanchez@wocu-monitoring.com>
2024-07-03 17:48:07 +0200

    Add the needed properties to the link_button components intantiated

--- python_web/views/links/links.py
+++ python_web/views/links/links.py
@@ -3,8 +3,8 @@ from python_web.components.link_button import link_button
 
 def links() -> rx.Component:
     return rx.vstack(
-        link_button(),
-        link_button(),
-        link_button(),
-        link_button(),
+        link_button("LinkedIn","https://www.linkedin.com/in/diegosanchezmartin/"),
+        link_button("GitHub","https://github.com/diegosanchezmartin"),
+        link_button("Sobre mi","https://www.google.es"),
+        link_button("Descargar CV", "https://www.google.es"),
     )
