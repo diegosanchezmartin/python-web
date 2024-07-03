3b637d1501adfe6c28232d27dc75e6b96a1e09d2
Diego Sánchez Martín <diego.sanchez@wocu-monitoring.com>
2024-07-03 17:08:44 +0200

    Import header component from views and create it

--- python_web/python_web.py
+++ python_web/python_web.py
@@ -1,11 +1,14 @@
 import reflex as rx
 from python_web.components.navbar import navbar
-
+from python_web.views.header import header
 class State(rx.State):
     pass
 
 def index() -> rx.Component:
-    return navbar()
+    return rx.vstack(
+        navbar(),
+        header()
+    )
 
 app = rx.App()
 app.add_page(index)
