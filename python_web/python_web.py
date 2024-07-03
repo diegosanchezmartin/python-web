ee322de433d88bac542866512595dc115587f639
Diego Sánchez Martín <diego.sanchez@wocu-monitoring.com>
2024-07-03 17:33:50 +0200

    Import links component and create it

--- python_web/python_web.py
+++ python_web/python_web.py
@@ -1,13 +1,15 @@
 import reflex as rx
 from python_web.components.navbar import navbar
 from python_web.views.header.header import header
+from python_web.views.links.links import links
 class State(rx.State):
     pass
 
 def index() -> rx.Component:
     return rx.vstack(
         navbar(),
-        header()
+        header(),
+        links()
     )
 
 app = rx.App()
