fe7ab8e1ac36eec13276c80788a3830e1644419c
Diego Sánchez Martín <diego.sanchez@wocu-monitoring.com>
2024-07-04 16:02:02 +0200

    Add the body text to all the link_button components inside links.py

--- python_web/views/links/links.py
+++ python_web/views/links/links.py
@@ -3,9 +3,25 @@ from python_web.components.link_button import link_button
 
 def links() -> rx.Component:
     return rx.vstack(
-        link_button("LinkedIn","https://www.linkedin.com/in/diegosanchezmartin/"),
-        link_button("GitHub","https://github.com/diegosanchezmartin"),
-        link_button("Sobre mi","https://www.google.es"),
-        link_button("Descargar CV", "https://www.google.es"),
+        link_button(
+            "LinkedIn", 
+            "Perfil profesional de LinkedIn", 
+            "https://www.linkedin.com/in/diegosanchezmartin/"
+        ),
+        link_button(
+            "GitHub", 
+            "Repositorio de proyectos en GitHub", 
+            "https://github.com/diegosanchezmartin"
+        ),
+        link_button(
+            "Sobre mi", 
+            "Pagina web personal", 
+            "https://www.google.es"
+            ),
+        link_button(
+            "Descargar CV", 
+            "Enlace para descargar mi CV", 
+            "https://www.google.es"
+        ),
         width="100%"
     )
