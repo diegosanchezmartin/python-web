3b258e201e30e77ae2ee684e24b1c3d2b61ff940
Diego Sánchez Martín <diego.sanchez@wocu-monitoring.com>
2024-07-04 10:37:29 +0200

    Add footer component to the project

--- python_web/components/footer.py
+++ python_web/components/footer.py
@@ -1,4 +1,9 @@
 import reflex as rx
 
 def footer() -> rx.Component:
-    return 
+    return rx.vstack(
+        rx.image(
+            src="favicon.ico"
+        ),
+        rx.text("© 2022-2024 <\\\\diegosanchezmartin> by Diego Sánchez Martín. From El Bierzo to the World, with ♥.")
+    )
