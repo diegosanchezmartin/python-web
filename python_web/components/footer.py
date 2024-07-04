1bffe73101c021d1bee2f054cc5309ef9e531c29
Diego Sánchez Martín <diego.sanchez@wocu-monitoring.com>
2024-07-04 10:59:22 +0200

    Add logic to the footer date

--- python_web/components/footer.py
+++ python_web/components/footer.py
@@ -1,9 +1,15 @@
 import reflex as rx
+import datetime
 
 def footer() -> rx.Component:
+    current_year = datetime.datetime.now().year
     return rx.vstack(
         rx.image(
             src="favicon.ico"
         ),
-        rx.text("© 2022-2024 <\\\\diegosanchezmartin> by Diego Sánchez Martín. From El Bierzo to the World, with ♥.")
+        rx.link(
+            "© 2022-" + format(current_year) + "<\\\\diegosanchezmartin> by Diego Sánchez Martín.",
+            href="https://www.google.es",
+            is_external=True),
+        rx.text("From El Bierzo to the World, with ♥.")
     )
