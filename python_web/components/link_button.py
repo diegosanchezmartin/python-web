e83546634c931a9a1553244e8e9795299f22f8b6
Diego Sánchez Martín <diego.sanchez@wocu-monitoring.com>
2024-07-04 16:02:59 +0200

    Add property to link_button to describe the body of the button and change its structure

--- python_web/components/link_button.py
+++ python_web/components/link_button.py
@@ -1,16 +1,19 @@
 import reflex as rx
+import python_web.styles.styles as styles
 
-def link_button(text: str, url: str) -> rx.Component:
+def link_button(title: str, body: str, url: str) -> rx.Component:
     return rx.link(
         rx.button(
             rx.hstack(
                 rx.icon(
                     tag="arrow_right",
-                    size=20,
+                    width=styles.Size.LARGE.value,
+                    height=styles.Size.LARGE.value,
                 ),
                 rx.vstack(
-                    rx.text(text),
-                    rx.text(text),
+                    rx.text(title, style=styles.button_title_style),
+                    rx.text(body, style=styles.button_body_style),
+                    align_items="start"
                 ),
             ),
         ),
