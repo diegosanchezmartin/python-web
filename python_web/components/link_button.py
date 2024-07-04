9160dc191adf0133fdbd2c3e6f590284bd735a93
Diego Sánchez Martín <diego.sanchez@wocu-monitoring.com>
2024-07-04 15:20:53 +0200

    Change stucture of the link_button component to show 2 texts inside a vstack component and add an icon to it

--- python_web/components/link_button.py
+++ python_web/components/link_button.py
@@ -2,7 +2,20 @@ import reflex as rx
 
 def link_button(text: str, url: str) -> rx.Component:
     return rx.link(
-        rx.button(text),
+        rx.button(
+            rx.hstack(
+                rx.icon(
+                    tag="arrow_right",
+                    size=10,
+                ),
+                rx.vstack(
+                    rx.text(text),
+                    rx.text(text),
+                ),
+            ),
+            width="100%"
+        ),
         href=url,
-        is_external=True
+        is_external=True,
+        width="100%"
     )
