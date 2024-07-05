49f291a8f4488204e9d6ef74deb17f66c29536f4
Diego Sánchez Martín <diego.sanchez@wocu-monitoring.com>
2024-07-05 16:53:21 +0200

    Add color and change the text structure from the navbar component

--- python_web/components/navbar.py
+++ python_web/components/navbar.py
@@ -1,13 +1,18 @@
 import reflex as rx
 from python_web.styles.styles import Size as Size
+from python_web.styles.colors import Color as Color
+from python_web.styles.colors import TextColor as TextColor
 
 def navbar() -> rx.Component:
     return rx.hstack(
-        rx.text(
-            "</diegosanchezmartin>",
+        rx.box(
+            rx.chakra.span(
+                "</diegosanchezmartin>",
+                color = TextColor.HEADER.value,
+            ),
         ),
         position ="sticky",
-        bg="lightgray",
+        bg=Color.CONTENT.value,
         padding_x=Size.DEFAULT.value,
         padding_y=Size.SMALL.value,
         z_index="999",
