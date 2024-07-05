849be4abfe992de24becb245963d4606a40cd1b1
Diego Sánchez Martín <diego.sanchez@wocu-monitoring.com>
2024-07-05 08:54:22 +0200

    Import title to the links file and instantiate 3 titles for the different links

--- python_web/views/links/links.py
+++ python_web/views/links/links.py
@@ -1,8 +1,10 @@
 import reflex as rx
 from python_web.components.link_button import link_button
+from python_web.components.title import title
 
 def links() -> rx.Component:
     return rx.vstack(
+        title("¡Conecta conmigo!"),
         link_button(
             "LinkedIn", 
             "Perfil profesional de LinkedIn", 
@@ -13,11 +15,13 @@ def links() -> rx.Component:
             "Repositorio de proyectos en GitHub", 
             "https://github.com/diegosanchezmartin"
         ),
+        title("Conoce mi historia"),
         link_button(
             "Sobre mi", 
             "Pagina web personal", 
             "https://www.google.es"
             ),
+        title("Recursos disponibles"),
         link_button(
             "Descargar CV", 
             "Enlace para descargar mi CV", 
