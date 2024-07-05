cd89fff32742779576aac938f20ba9c6719052e8
Diego Sánchez Martín <diego.sanchez@wocu-monitoring.com>
2024-07-05 14:26:07 +0200

    Change font_size and margin from footer component

--- python_web/components/footer.py
+++ python_web/components/footer.py
@@ -1,5 +1,6 @@
 import reflex as rx
 import datetime
+from python_web.styles.styles import Size as Size
 
 def footer() -> rx.Component:
     current_year = datetime.datetime.now().year
@@ -10,6 +11,13 @@ def footer() -> rx.Component:
         rx.link(
             "© 2022-" + format(current_year) + " <\\\\diegosanchezmartin> by Diego Sánchez Martín.",
             href="https://www.google.es",
-            is_external=True),
-        rx.text("From El Bierzo to the World, with ♥.")
+            is_external=True,
+            font_size = Size.MEDIUM.value,
+        ),
+        rx.text(
+            "From El Bierzo to the World, with ♥.",
+            font_size = Size.MEDIUM.value,
+            margin_top="0px !important",
+        ),
+        margin_bottom = Size.BIG.value,
     )
