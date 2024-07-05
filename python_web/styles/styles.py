b7e65f2168500e4fcc93e8e941942e623e5ec8f3
Diego Sánchez Martín <diego.sanchez@wocu-monitoring.com>
2024-07-05 08:49:01 +0200

    Add the style of the tittle to the styles file

--- python_web/styles/styles.py
+++ python_web/styles/styles.py
@@ -27,6 +27,12 @@ BASE_STYLE = {
     }
 }
 
+title_style = dict(
+    size="lg",
+    width="100%",
+    padding_top=Size.DEFAULT.value
+)
+
 button_title_style = dict(
     font_size = Size.DEFAULT.value
 )
