bffdf9e2a7ec34cbcbdc9165d2e40847174156d9
Diego Sánchez Martín <diego.sanchez@wocu-monitoring.com>
2024-07-05 14:59:47 +0200

    Fix a problem with the colors definition

--- python_web/styles/colors.py
+++ python_web/styles/colors.py
@@ -3,10 +3,10 @@ from enum import Enum
 class Color(Enum):
     PRIMARY = "#14A1F0"
     SECONDARY = "#087ec4"
-    BACKGROUND = "0C151D" 
+    BACKGROUND = "#0C151D" 
     CONTENT = "#171F26"
 
 class TextColor(Enum):
     HEADER = "#F1F2F4"
     BODY = "#C3C7CB"
-    FOOTER = "A3ABB2" 
+    FOOTER = "#A3ABB2" 
