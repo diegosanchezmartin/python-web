5565149d01995a052f0cf0e9bd81eadfe3d6dd2a
Diego Sánchez Martín <diego.sanchez@wocu-monitoring.com>
2024-07-05 13:29:19 +0200

    Add the chakra statement to span component in reflex because it is deprecated in the old version with rx.span

--- python_web/components/info_text.py
+++ python_web/components/info_text.py
@@ -3,7 +3,7 @@ from python_web.styles.styles import Size as Size
 
 def info_text(title:str, body: str) -> rx.Component:
     return rx.box(
-        rx.span(
+        rx.chakra.span(
             title,
             font_weight="bold",
             color="blue"
